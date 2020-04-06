8745efee7cd19574ec5f7fc98c2571bd29b06a4a
Anatoly Myachev <anatoly.myachev@intel.com>
2020-04-06 21:44:28 +0300

    fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .utils import (cod, combinate_requirements, compare_dataframes,
-                    convert_type_ibis2pandas, execute_process,
+                    convert_type_ibis2pandas, create_process, execute_process,
                     files_names_from_pattern, find_free_port,
                     import_pandas_into_module_namespace,
                     load_data_pandas, mse, print_times,
